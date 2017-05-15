42efadb555df0d1348e8ac52d12234d1059d38b1
richardo2016 <richardo2016@gmail.com>
2017-05-15 14:33:18 +0800

    add 'http' and 'logger' directory. upgrade to 0.0.2-alpha

---
+++ src/logger/index.js
@@ -0,0 +1,44 @@
+/**
+ * NC金融
+ * investsavior.com
+ */
+
+let {DEBUG = true} = global || window || {}
+
+export class Logger {
+  constructor(level) {
+    if (level === undefined) {
+      level = DEBUG ? 'debug' : 'info';
+    }
+    this.level = level;
+  }
+
+  debug(...args) {
+    if (this.level === 'debug') {
+      console.debug(this.time(), ...args);
+    }
+  }
+
+  info(...args) {
+    if (this.level === 'debug' || this.level === 'info') {
+      console.info(this.time(), ...args);
+    }
+  }
+
+  warn(...args) {
+    if (this.level === 'debug' || this.level === 'info' || this.level === 'warn') {
+      console.warn(this.time(), ...args);
+    }
+  }
+
+  error(...args) {
+    console.error(this.time(), ...args);
+  }
+
+  time() {
+    let d = new Date();
+    return d.toTimeString().substring(0, 8) + '.' + d.getMilliseconds();
+  }
+};
+
+export default new Logger();
