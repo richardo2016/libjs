1e1e7cc418306dccabd2884b3114ff75f06d8c83
richardo2016 <richardo2016@gmail.com>
2017-07-10 15:59:51 +0800

    - add script prepublishOnly. - little change for built-in.spec.js.

--- src/scaffold/webpack.js
+++ src/scaffold/webpack.js
@@ -1,3 +1,10 @@
+/**
+ * @apiDefine importDirectories
+ *            this function can make it easy to import one directory's file as modules, you can
+ *            filter the module_name, module's content before return the whole hash.
+ * @apiParam  require.context(): webpackRequireContext result from 'require.context(regex|string, ...)' in webpack runtime,
+ *            which has methods `keys` which returns one iterator for every files included
+ */
 export function importDirectories (webpackRequireContext = {}, options) {
   let direcotry = {}
   let {
